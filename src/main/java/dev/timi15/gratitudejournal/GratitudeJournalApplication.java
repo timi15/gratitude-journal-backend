151ec9b54e3daf7609b17efbc7fc18a2cb71bce6
Varga Timea <vargatimi505@mailbox.unideb.hu>
2025-06-17 14:47:17 +0200

    initial state

---
+++ src/main/java/dev/timi15/gratitudejournal/GratitudeJournalApplication.java
@@ -0,0 +1,13 @@
+package dev.timi15.gratitudejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratitudeJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratitudeJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/timi15/gratitudejournal/GratitudeJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.timi15.gratitudejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratitudeJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
