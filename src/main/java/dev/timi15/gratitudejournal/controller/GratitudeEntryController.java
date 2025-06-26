f3b42ec7a4819fc6b2af2df2a6fb3d4507eff52d
Varga Timea <vargatimi505@mailbox.unideb.hu>
2025-06-26 15:54:01 +0200

    add MediaType to all request mapping

--- src/main/java/dev/timi15/gratitudejournal/controller/GratitudeEntryController.java
+++ src/main/java/dev/timi15/gratitudejournal/controller/GratitudeEntryController.java
@@ -6,6 +6,7 @@ import dev.timi15.gratitudejournal.service.GratitudeEntryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,20 +20,20 @@ public class GratitudeEntryController {
 
     private final GratitudeEntryService gratitudeEntryService;
 
-    @GetMapping("/gratitudes")
+    @GetMapping(path = "/gratitudes", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<GratitudeEntryResponseDTO>> getAllGratitudeEntry() throws ExecutionException, InterruptedException {
         List<GratitudeEntryResponseDTO> gratitudeEntries = gratitudeEntryService.getAllGratitudeEntry();
         return ResponseEntity.ok(gratitudeEntries);
     }
 
 
-    @GetMapping("/gratitude/{id}")
+    @GetMapping(path = "/gratitude/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GratitudeEntryResponseDTO> getGratitudeEntryById(@PathVariable String id) throws ExecutionException, InterruptedException {
         GratitudeEntryResponseDTO gratitudeEntry = gratitudeEntryService.getGratitudeEntryById(id);
         return ResponseEntity.ok(gratitudeEntry);
     }
 
-    @PostMapping("/save")
+    @PostMapping(path = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createGratitudeEntry(@Valid @RequestBody GratitudeEntryRequestDTO gratitudeEntryRequestDTO) throws ExecutionException, InterruptedException {
         gratitudeEntryService.createGratitudeEntry(gratitudeEntryRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -44,7 +45,7 @@ public class GratitudeEntryController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PutMapping("/gratitude/{id}")
+    @PutMapping(path = "/gratitude/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> modifyGratitudeEntry(@PathVariable String id, @Valid @RequestBody GratitudeEntryRequestDTO gratitudeEntryRequestDTO) throws ExecutionException, InterruptedException {
         gratitudeEntryService.modifyGratitudeEntryById(id, gratitudeEntryRequestDTO);
         return ResponseEntity.status(HttpStatus.OK).build();
